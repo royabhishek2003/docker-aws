3733a4047ec058e288e36f64768c890191148999
ankurdotio <prajapatiankur8349@gmail.com>
2026-03-23 16:16:17 +0530

    integrate collaborative editing with Yjs and Monaco Editor

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css"
 import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import { useRef, useMemo } from "react"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
 
 function App() {
+
+  const editorRef = useRef(null)
+
+  const ydoc = useMemo(() => new Y.Doc(), [])
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc])
+
+
+  const handleMount = (editor) => {
+    editorRef.current = editor
+
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc,{
+      autoConnect: true,
+    })
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  }
 
   return (
     <main
@@ -17,6 +41,7 @@ function App() {
           defaultLanguage="javascript"
           defaultValue="// some comment"
           theme="vs-dark"
+          onMount={handleMount}
         />
       </section>
 
