4d7d4b9aaa0354533b646bca5200ebe511cf9ad5
ankurdotio <prajapatiankur8349@gmail.com>
2026-03-24 23:44:05 +0530

    add Dockerfile and .dockerignore; update SocketIOProvider URL and serve static files

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -45,7 +45,7 @@ function App() {
 
     if (username) {
 
-      const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      const provider = new SocketIOProvider("/", "monaco", ydoc, {
         autoConnect: true,
       })
 
